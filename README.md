450ba82d0539a9a53d03ab8790e42db5a1430f86
Sean Moore <sean.moore@thoughtexpress.com>
2018-05-24 20:06:57 +0200

    Fixed Heading order in output csv. Updated Readme

--- README.md
+++ README.md
@@ -4,24 +4,50 @@ Performs a GROUP BY on table data contained in a CSV
 
 ## Getting Started
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
 
 ### Prerequisites
 
 Python 3.6
 
-```
-Give examples
-```
 
-## Running
+### Running
 
-```
-python csv_aggregator
+The csv input and output files are passed in as command line arguments.
 
 ```
+python csv_aggregator.py name_of_input.csv name_of_output.csv
+
+```
+
+## Testing
+
+To test this, run
+
+```
+python csv_aggregator.py loans.csv output.csv
+
+```
+
+
+## Considerations
+
+* CSVAggregator, aggregates the rows of a csv file and performs a group by on a specified tuple.
+* Assumption is that the first row of csv contains column headings
+
+*The CSVAggregator object is initialized with the input and output file name.
+*A group by  can be performed by calling the group_by method of the object.
+*This takes in the columns that must by grouped ( a tuple) as well as a single column to perform COUNT and SUM on.
+
+##Limitations:
+*CSVAggregator can handle most groupby column names, however, to handle date, the heading must be specified as Date.
+*The group_by clause will then group the date by month and year.
+*group_by can only aggregate one field and only performs SUM and COUNT on that field, thus this field must be of type float or int.
+*The CSV Files must also be , delimited
+
+## Performance and Scaling
+* At present the function only performs single core processing on the data.
 
 
 ## Authors
 
-* **Sean Moore
+* Sean Moore
--- csv-aggregator.py
+++ csv-aggregator.py
@@ -2,22 +2,46 @@
 
 import datetime
 import csv
+import sys
+import logging
+
+# Create Logger
+logger = logging.getLogger('csv_aggregator')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("aggregator.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 class CSVAggregator:
     """
-        CSVAggregator, aggregates the rows of a csv file and performs a groupby on a specified tuple.
-        Assumption is that the first
+        CSVAggregator, aggregates the rows of a csv file and performs a group by on a specified tuple.
+        Assumption is that the first row of csv contains column headings
+
+        Limitations:
+            CSVAggregator can handle most group by headings, however, to handle date, the heading must be specified
+            as Date. The group by clause will then group the date by month and year.
+            Group by can only aggregate one field and only performs SUM and COUNT on that field, thus this field must be
+            of type float or int.
+
+            The CSV Files must also be , delimited
+
     """
-    __AGGREGATE_FIELDS = ['Count', 'Sum']
+    __AGGREGATE_FIELDS = ['Sum', 'Count']
 
     def __init__(self, input_file_location, output_file_location):
-        self._file_reader = open(input_file_location, "r")
-        self._file_writer = open(output_file_location, "w")
+        try:
+            self._file_reader = open(input_file_location, "r")
+            self._file_writer = open(output_file_location, "w")
+        except Exception as e:
+            logger.info("Error opening files")
+            logger.exception("Exception Message")
+            raise e
 
     @staticmethod
-    def get_record_stats(record, header_row, fields, aggregate_field, acc):
-        agg = float(record[header_row.index(aggregate_field)])
+    def aggregate(record, header_row, fields, agg_field, acc):
+        agg = float(record[header_row.index(agg_field)])
         tup = tuple(record[header_row.index(i)] for i in fields)
         if acc.get(tup):
             acc[tup]["sum"] += agg
@@ -28,28 +52,37 @@ class CSVAggregator:
             acc[tup]["count"] = 1
         return acc
 
+    '''
+        Returns Date in the Month, Year format
+    '''
+
     @staticmethod
     def return_date_grouping(date):
         d = datetime.datetime.strptime(date, "'%d-%b-%Y'")
         return d.strftime("'%b %Y'")
 
-    def group_by(self, group_by_fields, aggregate_field):
+    def group_by(self, group_by_args, aggregate_arg):
         acc = dict()
         reader = csv.reader(self._file_reader, delimiter=',')
-        fields = next(reader, None)
+        fields = [x.lower().strip() for x in next(reader, None)]
+        group_by_args = [x.lower().strip() for x in group_by_args]
+        aggregate_arg = aggregate_arg.strip().lower()
         for record in reader:
-            if "Date" in fields:
-                record[fields.index("Date")] = self.return_date_grouping(record[fields.index("Date")])
-            acc = self.get_record_stats(record, fields, group_by_fields, aggregate_field, acc)
+            # There is a special consideration for the date field in the record.
+            if "date" in fields:
+                record[fields.index("date")] = self.return_date_grouping(record[fields.index("date")])
+            acc = self.aggregate(record, fields, group_by_args, aggregate_arg, acc)
         self._file_reader.close()
         return acc
 
-    def aggregate_to_file(self, group_by_fields,aggregate_field):
+    def aggregate_to_file(self, group_by_arg, aggregate_arg):
         writer = csv.writer(self._file_writer, delimiter=',')
-        writer.writerow(group_by_fields + self.__AGGREGATE_FIELDS)
-        output = self.group_by(group_by_fields, aggregate_field)
+        writer.writerow(group_by_arg + self.__AGGREGATE_FIELDS)
+        output = self.group_by(group_by_arg, aggregate_arg)
+
         for key, value in output.items():
-            writer.writerow(key + tuple(value.values()))
+            writer.writerow(key + (value["sum"], value["count"]))
+        self._file_writer.close()
 
     def close_files(self):
         if self._file_reader:
@@ -61,5 +94,15 @@ class CSVAggregator:
 if __name__ == '__main__':
     group_by_fields = ['Product', 'Network', 'Date']
     aggregate_field = 'Amount'
-    aggregator = CSVAggregator(input_file_location="hello.csv", output_file_location="test.csv")
-    aggregator.aggregate_to_file(group_by_fields, aggregate_field)
+    try:
+        input_file_location = sys.argv[1]
+        output_file_location = sys.argv[2]
+        logger.info(
+            "Starting CSV Aggregator, with input file: " + input_file_location + " and output file: " + output_file_location)
+        aggregator = CSVAggregator(input_file_location, output_file_location)
+        aggregator.aggregate_to_file(group_by_fields, aggregate_field)
+        logger.info("Finished job")
+        aggregator.close_files()
+    except Exception as e:
+        logger.info("Error with file")
+        logger.exception("Exception Message")
